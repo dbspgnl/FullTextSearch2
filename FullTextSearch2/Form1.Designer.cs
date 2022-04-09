2f1db4001ae4fd9308e9212390ca63ff171ace6e
khokhoa <dbspgnl@naver.com>
2022-04-09 12:12:01 +0900

    init commit

---
+++ FullTextSearch2/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace FullTextSearch2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(261, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(373, 373);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.ColumnWidth = 500;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(3, 7);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(0, 3, 3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(367, 362);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(373, 29);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(267, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(106, 29);
+            this.panel3.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 402);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "FullTextSearch2";
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FullTextSearch2/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullTextSearch2
+{
+    public partial class Form1 : Form
+    {
+
+        /*
+            + 가로 스크롤 적용
+            + 직접 셀렉하면 색깔을 다르게 하고 Delete하면 해당 목록 지우기
+            + 많은 데이터를 검색할 때 스피너 적용
+            + 
+         */
+        string[] files = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listBox1.AllowDrop = true;
+            listBox1.DragDrop += listBoxFiles_DragDrop;
+            listBox1.DragEnter += listBoxFiles_DragEnter;
+            listBox1.DrawMode = DrawMode.OwnerDrawFixed;
+            listBox1.DrawItem += new DrawItemEventHandler(listBox1_DrawItem);
+            listBox1.HorizontalScrollbar = true;
+            textBox1.KeyDown += textBox1_KeyDown;
+
+        }
+
+        private void listBoxFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("listBoxFiles_DragEnter");
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+
+        }
+
+        private void listBoxFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            Debug.WriteLine("listBoxFiles_DragDrop");
+            files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                listBox1.Items.Add(file);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.SelectionMode = SelectionMode.MultiExtended; // 다중 선택 모드
+            string result = "";
+            string word = textBox1.Text;
+            List<int> indexList = new List<int>();
+
+            // selected 초기화
+            listBox1.SelectedIndex = -1;
+
+            foreach (var input_items in listBox1.Items)
+            {
+                // 파일 경로 담기
+                result += string.Format("{0} ", input_items);
+                string textValue = System.IO.File.ReadAllText(input_items.ToString());
+                Debug.WriteLine(textValue);
+                // 해당하는 인덱스 담기 
+                bool isContains = textValue.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0;
+                if (isContains)
+                {
+                    Debug.WriteLine("글자가 포함되어 있습니다.");
+                    indexList.Add(listBox1.Items.IndexOf(input_items));
+                }
+
+            }
+            Debug.WriteLine(indexList);
+            //해당하는 데이터 selected 하기
+            foreach (var index in indexList)
+            {
+                listBox1.SetSelected(index, true);
+            }
+
+            Debug.WriteLine("button1_Click(list): " + result);
+            Debug.WriteLine("button1_Click(word): " + word);
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+
+            bool isItemSelected = ((e.State & DrawItemState.Selected) == DrawItemState.Selected);
+            int itemIndex = e.Index;
+            if (itemIndex >= 0 && itemIndex < listBox1.Items.Count)
+            {
+                Graphics g = e.Graphics;
+
+                // Background Color
+                SolidBrush backgroundColorBrush = new SolidBrush((isItemSelected) ? Color.Red : Color.White);
+                g.FillRectangle(backgroundColorBrush, e.Bounds);
+
+                // Set text color
+                string itemText = listBox1.Items[itemIndex].ToString();
+
+                SolidBrush itemTextColorBrush = (isItemSelected) ? new SolidBrush(Color.White) : new SolidBrush(Color.Black);
+                g.DrawString(itemText, e.Font, itemTextColorBrush, listBox1.GetItemRectangle(itemIndex).Location);
+
+                // Clean up
+                backgroundColorBrush.Dispose();
+                itemTextColorBrush.Dispose();
+            }
+
+            e.DrawFocusRectangle();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(sender, e);
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+
+        private void DisplayHScroll()
+        {
+            // 부분적으로 표시되는 항목이 없는지 확인합니다.
+            listBox1.IntegralHeight = true;
+
+            // ListBox에 너비가 넓은 항목을 추가합니다.
+            for (int x = 0; x < 10; x++)
+            {
+                listBox1.Items.Add("Item  " + x.ToString() + " is a very large value that requires scroll bars");
+            }
+
+            // 가로 스크롤 막대를 표시합니다.
+            listBox1.HorizontalScrollbar = true;
+
+            // ListBox에서 가장 큰 항목의 크기를 결정할 때 사용할 Graphics 개체를 만듭니다.
+            Graphics g = listBox1.CreateGraphics();
+
+            // 목록의 마지막 항목을 사용하여 MeasureString 메서드를 사용하여 HorizontalExtent의 크기를 결정합니다.
+            int hzSize = (int)g.MeasureString(listBox1.Items[listBox1.Items.Count - 1].ToString(), listBox1.Font).Width;
+            // HorizontalExtent 속성을 설정합니다.
+            listBox1.HorizontalExtent = hzSize;
+        }
+
+
+    }
+}
